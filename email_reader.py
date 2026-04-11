959db4ed6b67a03e773781d3ac532a3e15a7585a
viktoriia_kychan <viktoriia.kychan@gmail.com>
2026-04-11 13:35:42 +0100

    Add a menu and impelent it for the [1] - Read emails from the last N days

--- email_reader.py
+++ email_reader.py
@@ -11,13 +11,23 @@ password = os.getenv("PASSWORD")
 imap_server = os.getenv("IMAP_SERVER")
 
 
-def get_old_unread_emails(days=30):
+def get_unread_olderThan_n_emails(days=30):
     date_limit = (datetime.now() - timedelta(days=days)).date()
     with MailBox(imap_server).login(email, password) as mailbox:
-        emails = mailbox.fetch(AND(seen = False, date_lt= date_limit), limit = 10, reverse=True)
+        #emails = mailbox.fetch(AND(seen = False, date_lt = date_limit), limit = 10, reverse=True, bulk=True) # emails whose date is before this date
         # for e in emails:
         #     print(e.subject)
-        return [msg.uid for msg in emails] # returns the id of the emails
+        uids = mailbox.uids(AND(date_lt=date_limit, seen = False))
+        return uids
+        #return [msg.uid for msg in emails] # returns the id of the emails
+
+def get_emails_last_n_days(days=30):
+    date_limit = (datetime.now() - timedelta(days=days)).date()
+
+    with MailBox(imap_server).login(email, password) as mailbox:
+        uids = mailbox.uids(AND(date_gte=date_limit, seen = False))
+        #return [msg.uid for msg in emails]
+        return uids
 
 def mark_emails_as_read(uids):
     with MailBox(imap_server).login(email, password) as mailbox:
--- main.py
+++ main.py
@@ -1,3 +1,36 @@
-from email_reader import read_unread_emails
+import email_reader
+import menu
 
-read_unread_emails(days = 10)
+def main():
+    while True:
+        menu.show_menu()
+        choice = input("Enter option [1-4]: ").strip()
+
+        if choice == "1":
+            print("You chose the option \"[1] - Read emails from the last N days\"\n")
+
+            days = int(input("Enter number of days: "))
+            uids = email_reader.get_emails_last_n_days(days)
+            menu.loading_bar()
+            email_reader.mark_emails_as_read(uids)
+            #print("Emails found:", uids)
+
+        elif choice == "2":
+            days = int(input("Enter number of days: "))
+            uids = email_reader.get_old_unread_emails(days)
+            print("Emails found:", uids)
+
+        elif choice == "3":
+            sender = input("Enter sender email: ")
+            email_reader.delete_by_sender(sender)
+
+        elif choice == "4":
+            print("Exiting Email Cleaner. Goodbye")
+            break
+
+        else:
+            print("Invalid option. Try again.\n")
+
+
+if __name__ == "__main__":
+    main()
